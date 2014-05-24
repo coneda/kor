689f1d5bb1ba32fc75ab92ba441139977aa2eea6
Moritz Schepp <moritz.schepp@gmail.com>
2014-05-24 21:19:45 +0200

    removed redundant call to media preview task

--- deploy/post-install.sh
+++ deploy/post-install.sh
@@ -2,7 +2,6 @@
 
 su -c "cd /opt/kor/current ; RAILS_ENV=production bundle exec rake db:setup" kor
 su -c "cd /opt/kor/current && bundle exec rake assets:precompile" kor
-su -c "cd /opt/kor/current && bundle exec rake kor:deploy:media_preview" kor
 su -c "touch /opt/kor/current/tmp/restart.txt" kor
 
 /etc/init.d/sunspot start
