a19c6e7d1fd62cda7ebd275849a3647b2301e23e
Moritz Schepp <moritz.schepp@gmail.com>
2014-09-09 01:40:14 +0200

    added first version of LDAP auth (not fully tested)

--- lib/auth/authentication.rb
+++ lib/auth/authentication.rb
@@ -2,18 +2,38 @@ module Auth::Authentication
 
   def self.authenticate(username, password)
     user = User.authenticate username, password
-    user ? username : nil
+    return true if user
+
+    (Kor.config["auth.sources"] || []).each do |method, c|
+      data = `KOR_USERNAME=#{username} KOR_PASSWORD=#{password} #{c["script"]}`
+      status = $?.exitstatus
+      if status == 0
+        return JSON.parse(data).merge(
+          :parent_username => c["map_to"]
+        )
+      end
+    end
+
+    false
   end
   
-  def self.authorize(username)
-    User.find_by_name(username, :include => :groups) || User.create(
+  def self.authorize(username, additional_attributes = true)
+    user = User.find_by_name(username, :include => :groups) || User.new(
       :name => username
     )
+
+    if additional_attributes.is_a?(Hash)
+      user.update_attributes additional_attributes
+    else
+      user.save
+    end
+
+    user
   end
   
   def self.login(username, password)
-    if authenticate(username, password)
-      authorize(username)
+    if attributes = authenticate(username, password)
+      authorize(username, attributes)
     end
   end
   
--- spec/lib/auth/authentication_spec.rb
+++ spec/lib/auth/authentication_spec.rb
@@ -4,10 +4,19 @@ describe Auth::Authentication do
 
   it "should create users when they don't exist" do
     expect(User).to receive(:generate_password).exactly(:once)
-
     user = described_class.authorize("jdoe")
 
     expect(user.name).to eq("jdoe")
+  end
+
+  it "should call external auth scripts" do
+    FactoryGirl.create :user, :name => "example_auth"
+
+    expect(described_class.login "jdoe", "wrong").to be_false
+    expect(described_class.login "jdoe", "123456").to be_true
+
+    expect(User.count).to eq(2)
+    expect(User.last.parent_username).to eq("example_auth")
   end
 
 end
