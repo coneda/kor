115bf64ae9c577b37e8dff5b7e6756140026012e
Moritz Schepp <moritz.schepp@gmail.com>
2015-11-11 17:12:39 +0100

    fixes selectors for cucumber stories

--- features/support/selectors.rb
+++ features/support/selectors.rb
@@ -27,10 +27,10 @@ module HtmlSelectorsHelpers
     # This allows you to provide a quoted selector as the scope
     # for "within" steps as was previously the default for the
     # web steps:
-    # when /"(.+)"/
-    #   $1
     when /^the first relation on the page$/
       [:css, ".relation"]
+    when /"(.+)"/
+      $1
     else
       raise "Can't find mapping from \"#{locator}\" to a selector.\n" +
         "Now, go and add a mapping in #{__FILE__}"
