566fc40fc8e4b59b9a751e40c84444322df06f40
Moritz Schepp <moritz.schepp@gmail.com>
2015-12-13 17:44:10 +0100

    fixes deprecation errors with mime_types gem

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -5,10 +5,12 @@
 # Mime::Type.register_alias "text/html", :iphone
 # Mime::Type.register "text/html", :mobile
 
+# required for paperclip to make it accept the .image file extension
 ["image/tiff", "image/jpeg", "image/png", "image/gif", "image/vnd.adobe.photoshop"].each do |mtn|
   if mt = MIME::Types[mtn].first
-    mt.extensions << "image"
-    MIME::Types.index_extensions mt
+    mt.add_extensions "image"
+    # mt.extensions << "image"
+    # MIME::Types.send :index_extensions, mt
   else
     puts mtn
   end
