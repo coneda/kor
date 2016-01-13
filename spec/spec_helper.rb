0ade4b73aef93a3e44b72d9fa76c7ac90b83eb08
Moritz Schepp <moritz.schepp@gmail.com>
2016-01-13 15:10:09 +0100

    added binstubs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,7 +22,3 @@ VCR.configure do |c|
   c.default_cassette_options = {:record => :new_episodes}
   c.allow_http_connections_when_no_cassette = true
 end
-
-VCR.cucumber_tags do |t|
-  t.tag "@vcr", :use_scenario_name => true
-end
