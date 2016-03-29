7a9eb157e12f3588335c75f993863bcc2abae9b2
Moritz Schepp <moritz.schepp@gmail.com>
2016-03-29 17:10:52 +0200

    integrated docker building and Dockerfile(s)

---
+++ deploy/dockerize.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+COMMIT=$1
+RAILS_ENV=$2
+
+RUBY_VERSION=$(git show master:.ruby-version)
+
+git archive $COMMIT > tmp/kor.tar
+
+sudo docker run --rm \
+  -v $(pwd)/deploy/docker/Dockerfile.erb:/template.erb \
+  ruby:$RUBY_VERSION \
+  erb ruby_version=$RUBY_VERSION rails_env=$RAILS_ENV /template.erb \
+  > tmp/kor.dockerfile
+
+sudo docker build \
+  --rm=true \
+  --force-rm \
+  -f tmp/kor.dockerfile \
+  -t docker.coneda.net:443/kor:$COMMIT \
+  .
