a50b3bece3fc5e701132223ac4f34842b76c105f
Moritz Schepp <moritz.schepp@gmail.com>
2016-05-24 15:17:29 +0200

    removed mail server configuration from web interface

--- features/step_definitions/config_steps.rb
+++ features/step_definitions/config_steps.rb
@@ -1,5 +1,5 @@
 Then /^the application config file should include "([^"]*)" with "([^"]*)"$/ do |key, value|
-  expect(Kor::Config.new(Kor.app_config_file)[key]).to eq(value)
+  expect(Kor::Config.new(Kor::Config.app_config_file)[key]).to eq(value)
 end
 
 Given(/^the config option "([^"]*)" is set to "([0-9\.]+)"$/) do |key, float_value|
