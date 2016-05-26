b67a260d98e205f1503df89a9148db09afb296e2
Moritz Schepp <moritz.schepp@gmail.com>
2016-05-26 19:08:45 +0200

    fixes loading of translations bug

--- app/controllers/api/public_controller.rb
+++ app/controllers/api/public_controller.rb
@@ -4,6 +4,8 @@ class Api::PublicController < BaseController
     @current_entities = Entity.
       includes(:medium).
       find_all_by_id_keep_order(session[:current_history])
+
+    I18n.backend.load_translations
   end
     
 end
