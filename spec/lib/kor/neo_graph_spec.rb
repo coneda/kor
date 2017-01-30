41a19dd9eb9ad2e162892a013da600350da65cfd
Moritz Schepp <moritz.schepp@gmail.com>
2017-01-30 12:35:54 +0100

    made neo4j interface more clean

--- spec/lib/kor/neo_graph_spec.rb
+++ spec/lib/kor/neo_graph_spec.rb
@@ -1,23 +1,36 @@
 require "rails_helper"
 
-describe Kor::NeoGraph do
+describe Kor::NeoGraph, skip: 'not integrated within the dependencies yet' do
 
   it "should create nodes" do
-    skip 'not integrated within the dependencies yet'
 
     admin = FactoryGirl.create :admin
     graph = described_class.new(admin)
 
     mona_lisa = FactoryGirl.create :mona_lisa
-    graph.store mona_lisa
+    graph.store_entity mona_lisa
 
     der_schrei = FactoryGirl.create :der_schrei
-    graph.store der_schrei
+    graph.store_entity der_schrei
 
     # node = graph.find(mona_lisa)
     # expect(node["entity_id"]).to eq(mona_lisa.id)
 
     # binding.pry
+  end
+
+  it 'should rollback transactions' do
+    admin = FactoryGirl.create :admin
+    mona_lisa = FactoryGirl.create :mona_lisa
+    graph = described_class.new(admin)
+
+    expect{
+      graph.transaction do
+        expect{
+          graph.store_entity mona_lisa
+        }.to change{graph.node_count}.by(1)
+      end
+    }.not_to change{graph.node_count}
   end
 
 end
