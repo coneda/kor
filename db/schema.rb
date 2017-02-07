2a8d96e61456100c2952318a97f9e9a832a8ff27
Moritz Schepp <moritz.schepp@gmail.com>
2017-02-07 11:53:43 +0100

    removed duplicate migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170131122409) do
+ActiveRecord::Schema.define(version: 20160926101319) do
 
   create_table "authority_group_categories", force: :cascade do |t|
     t.integer  "lock_version", limit: 4
