08a9ff1c9d9849ac19ec49334c2c33d8836b12fa
Moritz Schepp <moritz.schepp@gmail.com>
2017-08-13 00:25:05 +0200

    switched js error reporting off for testing

--- features/support/kor.rb
+++ features/support/kor.rb
@@ -35,7 +35,7 @@ end
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app,
     # debug: true,
-    js_errors: true,
+    js_errors: false,
     inspector: false
   )
 end
