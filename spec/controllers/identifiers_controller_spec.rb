6ce649ba9aa973f880102490bd17d8565bd04b1f
Moritz Schepp <moritz.schepp@gmail.com>
2019-04-11 10:23:31 -0700

    fixes #232

--- spec/controllers/identifiers_controller_spec.rb
+++ spec/controllers/identifiers_controller_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
 describe IdentifiersController, :type => :controller do
+  render_views
 
   it "should resolve an entity by id or uuid" do
     people = FactoryGirl.create :people, :fields => [
@@ -35,4 +36,18 @@ describe IdentifiersController, :type => :controller do
     expect(response).to redirect_to("/blaze#/entities/#{leonardo.id}")
   end
 
+  it 'should resolve an entity by a identifier value including a dot' do
+    people = FactoryGirl.create :people, :fields => [
+      Field.new(:name => "gnd_id", :is_identifier => true, :show_label => "GND-ID")
+    ]
+    leonardo = FactoryGirl.create :leonardo, :dataset => {"gnd_id" => "BMN_2002.33a-b_view1_bw"}
+
+    get :resolve, kind: 'gnd_id', id: "BMN_2002.33a-b_view1_bw"
+    expect(response).to redirect_to("/blaze#/entities/#{leonardo.id}")
+  end
+
+  it 'should resolve to a 404 page when the identifier is not found' do
+    get :resolve, kind: 'x', id: "1234"
+    expect(response).to be_a_not_found
+  end
 end
