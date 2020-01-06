0be36a35baec2b299df50a27bde479a8dc0faa7e
Moritz Schepp <moritz.schepp@gmail.com>
2020-01-06 15:39:11 -0800

    fixes locale tests

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -4,27 +4,31 @@ class UserMailer < ActionMailer::Base
 
   def reset_password(user)
     @user = user
-    I18n.locale = locale(@user)
-    mail(:to => user.email, :subject => I18n.t("mailer.subjects.password_reset"))
+    I18n.with_locale locale(@user) do
+      mail(:to => user.email, :subject => I18n.t("mailer.subjects.password_reset"))
+    end
   end
 
   def account_created(user)
     @user = user
-    I18n.locale = locale(@user)
-    mail(:to => user.email, :subject => I18n.t("mailer.subjects.account_created"))
+    I18n.with_locale locale(@user) do
+      mail(:to => user.email, :subject => I18n.t("mailer.subjects.account_created"))
+    end
   end
   
   def upcoming_expiry(user)
     @user = user
-    I18n.locale = locale(@user)
-    mail(:to => user.email, :subject => I18n.t("mailer.subjects.upcoming_expiry"))
+    I18n.with_locale locale(@user) do
+      mail(:to => user.email, :subject => I18n.t("mailer.subjects.upcoming_expiry"))
+    end
   end
   
   def download_ready(download)
     @user = download.user
-    I18n.locale = locale(@user)
-    @download = download
-    mail(:to => @user.email, :subject => I18n.t("mailer.subjects.download_ready"))
+    I18n.with_locale locale(@user) do
+      @download = download
+      mail(:to => @user.email, :subject => I18n.t("mailer.subjects.download_ready"))
+    end
   end
 
   
