6a254c1ebb4a59ab4b73891486a2f3e527a60dc4
Moritz Schepp <moritz.schepp@gmail.com>
2020-12-28 20:12:39 -0800

    fixes mirador link check

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -333,7 +333,7 @@ end
 
 Then(/^I should see mirador link with a usable href$/) do
   href = find_link('Mirador')['href']
-  expect(href).to match(/^http:\/\/127.0.0.1:\d+\/mirador\?manifest=http:\/\/127.0.0.1:\d+\/mirador\/\d+$/)
+  expect(href).to match(/^http:\/\/127.0.0.1:\d+\/mirador\?id=\d+&manifest=http:\/\/127.0.0.1:\d+\/mirador\/\d+$/)
 end
 
 When("I select autocomplete option {string}") do |string|
