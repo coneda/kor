f65c1345132a1590629494ab193bd8aa9f4e0a84
Moritz Schepp <moritz.schepp@gmail.com>
2021-05-26 21:17:06 -0700

    fixes ffmpeg to overwrite files without asking

--- lib/paperclip_processors/video.rb
+++ lib/paperclip_processors/video.rb
@@ -17,7 +17,7 @@ class Paperclip::Video < Paperclip::Processor
 
     outfile = "#{Kor.tmp_path}.mp4"
     args = "-c:v libx264 -crf 28 -c:a aac -b:a 256k -strict experimental -profile:v baseline -level 3.0 -movflags +faststart"
-    args = "-loglevel panic -nostats -i #{file.path} #{args} #{outfile}"
+    args = "-loglevel panic -nostats -i #{file.path} #{args} -y #{outfile}"
     Paperclip.run('ffmpeg', args)
     File.open(outfile)
   end
@@ -25,7 +25,7 @@ class Paperclip::Video < Paperclip::Processor
   def self.make_ogg(file, attachment)
     outfile = "#{Kor.tmp_path}.ogv"
     args = "-c:v libtheora -qscale:v 7 -c:a libvorbis -qscale:a 7"
-    args = "-loglevel panic -nostats -i #{file.path} #{args} #{outfile}"
+    args = "-loglevel panic -nostats -i #{file.path} #{args} -y #{outfile}"
     Paperclip.run('ffmpeg', args)
     File.open(outfile)
   end
@@ -33,14 +33,14 @@ class Paperclip::Video < Paperclip::Processor
   def self.make_webm(file, attachment)
     outfile = "#{Kor.tmp_path}.webm"
     args = "-c:v libvpx -crf 10 -b:v 1M -c:a libvorbis -qscale:a 7"
-    args = "-loglevel panic -nostats -i #{file.path} #{args} #{outfile}"
+    args = "-loglevel panic -nostats -i #{file.path} #{args} -y #{outfile}"
     Paperclip.run('ffmpeg', args)
     File.open(outfile)
   end
 
   def self.make_still(file, attachment)
     outfile = "#{Kor.tmp_path}.jpg"
-    args = "-ss 00:00:05 -i #{file.path} -vframes 1 #{outfile}"
+    args = "-ss 00:00:05 -i #{file.path} -vframes 1 -y #{outfile}"
     Paperclip.run('ffmpeg', args)
     attachment.instance.image = File.open(outfile)
   end
